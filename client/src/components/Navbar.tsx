3372b01c57406f889457d4528eccef2ac74b85b6
Toby Chin <92127289+spagechi@users.noreply.github.com>
2026-04-15 20:21:37 -0700

    Add functioning mobile navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -26,19 +26,51 @@ const Navbar = () => {
     }, [mobile]);
 
     return (
-        <header className="flex justify-between items-center h-34 pr-11 pl-12 bg-[#00588C] text-white">
-            <span className="text-xl">LOREM IPSUM</span>
-            <nav className="hidden sm:flex gap-12 ml-8">
-                {navLinks.map((link, key) => (
-                    <a key={key} href={link.href}>
-                        {link.label}
-                    </a>
-                ))}
-            </nav>
-            <button onClick={toggleMenu} className="sm:hidden">
-                {menuOpen ? <Close /> : <Menu />}
-            </button>
-        </header>
+        <div>
+            <header className="flex justify-between items-center h-34 pr-11 pl-12 bg-[#00588C] text-white">
+                <span className="text-xl">LOREM IPSUM</span>
+                <nav className="hidden sm:flex gap-12 ml-8">
+                    {navLinks.map((link, key) => (
+                        <a key={key} href={link.href}>
+                            {link.label}
+                        </a>
+                    ))}
+                </nav>
+                <button onClick={toggleMenu} className="sm:hidden">
+                    <Menu />
+                </button>
+            </header>
+
+            <div
+                onClick={toggleMenu}
+                className={`fixed inset-0 bg-black/50 z-2 transition-opacity duration-300 ${
+                menuOpen ? "opacity-100 visible" : "opacity-0 invisible"
+                }`}
+            />
+
+            <div
+                className={`${!mobile && "hidden"} fixed top-0 right-0 w-auto h-full max-w-sm pl-20 bg-white text-black z-3 transform transition-transform duration-300 ${
+                menuOpen ? "translate-x-0" : "translate-x-full"
+                }`}
+            >
+                <div className="flex flex-col items-end gap-8 pt-14 pr-11 text-right">
+                    <button onClick={toggleMenu} className="text-black">
+                        <Close />
+                    </button>
+
+                    {navLinks.map((link, key) => (
+                        <a
+                            key={key}
+                            href={link.href}
+                            onClick={toggleMenu}
+                            className="text-lg"
+                        >
+                            {link.label}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </div>        
     );
 };
 
